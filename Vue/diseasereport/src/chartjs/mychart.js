b91bfd410781ad9bc008f392ffb9eef5551af34d
MeBigWhite <1500674954@qq.com>
2020-05-27 10:09:47 +0800

    前端数据展示固定的部分
    未添加前后端交互的部分

---
+++ Vue/diseasereport/src/chartjs/mychart.js
@@ -0,0 +1,78 @@
+
+
+import echarts from 'echarts'
+const install = function(Vue) {
+    Object.defineProperties(Vue.prototype, {
+        $chart: {
+            get() {
+                return {
+                    
+                    //画一条简单的线
+                    line1: function (id) {
+                        this.chart = echarts.init(document.getElementById(id));
+                        this.chart.clear();
+
+                        const optionData = {
+                            legend: {},
+                            tooltip: {},
+                            dataset: {
+                                source: [
+                                    ['product', '2015', '2016', '2017'],
+                                    ['Matcha Latte', 43.3, 85.8, 93.7],
+                                    ['Milk Tea', 83.1, 73.4, 55.1],
+                                    ['Cheese Cocoa', 86.4, 65.2, 82.5],
+                                    ['Walnut Brownie', 72.4, 53.9, 39.1]
+                                ]
+                            },
+                            xAxis: {type: 'category'},
+                            yAxis: {},
+                            // Declare several bar series, each will be mapped
+                            // to a column of dataset.source by default.
+                            series: [
+                                {type: 'bar'},
+                                {type: 'bar'},
+                                {type: 'bar'}
+                            ]
+                        };
+
+                        this.chart.setOption(optionData);
+                    },
+                    //柱形图
+                    zhu: function (id) {
+                        this.chart = echarts.init(document.getElementById(id));
+                        this.chart.clear();
+
+                        const optionData = {
+                            legend: {},
+                            tooltip: {},
+                            dataset: {
+                                source: [
+                                    ['product', '轻微', '严重', '健康'],
+                                    ['20200518', 43, 8, 193],
+                                    ['20200519', 83, 7, 193],
+                                    ['20200520', 86, 6, 182],
+                                    ['20200521', 72, 5, 199]
+                                ]
+                            },
+                            xAxis: {type: 'category'},
+                            yAxis: {},
+                            // Declare several bar series, each will be mapped
+                            // to a column of dataset.source by default.
+                            series: [
+                                {type: 'bar'},
+                                {type: 'bar'},
+                                {type: 'bar'}
+                            ]
+                        };
+
+                        this.chart.setOption(optionData);
+                    },
+                }
+            }
+        }
+    })
+}
+
+export default {
+    install
+}
---
+++ Vue/diseasereport/src/views/Getbyday.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="getbyday">
+        <div id="pick"> 
+            <Daypick/>
+            <h1>近四天的我校的数据</h1>
+        </div>
+        <div id="chart1"></div>
+    </div>
+</template>
+
+<script>
+import Daypick from "../components/Daypick.vue"
+var echarts = require('echarts');
+
+export default {
+    name:'Getbyday',
+    components:{
+        Daypick,
+    },
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+        this.$chart.zhu('chart1');
+    }
+}
+</script>
+
+<style scoped>
+#getbyday{
+    width:80vw;
+    height:100vh;
+}
+#chart1{
+    width:80%;
+    height:80%;
+    margin:0 auto ;
+}
+#pick{
+    margin: 0 auto !important;
+}
+</style>
